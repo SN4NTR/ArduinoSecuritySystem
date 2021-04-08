0e2a55e61cae1013a989b3b71d57e01f154ab55a
Aliaksandr Miron <alex.miron1999@gmail.com>
2021-04-08 15:38:36 +0300

    Add I2C expander for LCD

--- Arduino/code/code.ino
+++ Arduino/code/code.ino
@@ -1,35 +1,47 @@
 #include <SoftwareSerial.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 #include "DHT.h"
-#include "LiquidCrystal.h"
 
 #define RED_LED_PIN 2
 #define GREEN_LED_PIN 3
+
 #define DHT_PIN 4
 #define DHT_TYPE DHT11
+
 #define GAS_SENSOR_PIN 9
 #define FLAME_SENSOR_PIN 10
 #define SOUNDER_PIN 11
-#define D7 53
-#define D6 51
-#define D5 49
-#define D4 47
-#define EN 45
-#define RS 43
+
+#define I2C_ADRESS 0x27
+#define LCD_ROWS_AMOUNT 4
+#define LCD_COLUMNS_AMOUNT 20
 
 #define RX_PIN 13
 #define TX_PIN 8
 
 String telephoneNumber = "12345678";
-float sensorValue;
 boolean isSmsSent;
+
+int flameValue;
+int gasValue;
+int temperature;
+int humidity;
 
 SoftwareSerial gsmSerial(RX_PIN, TX_PIN);
 DHT dht(DHT_PIN, DHT_TYPE);
-LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
+LiquidCrystal_I2C lcd(I2C_ADRESS, LCD_COLUMNS_AMOUNT, LCD_ROWS_AMOUNT);
 
 void setup() {
   Serial.begin(9600);
 
+  initPins();
+  initGsm();
+  initDhtSensor();
+  initLcd();
+}
+
+void initPins() {
   pinMode(FLAME_SENSOR_PIN, INPUT);
   pinMode(GAS_SENSOR_PIN, INPUT);
   pinMode(DHT_PIN, INPUT);
@@ -37,15 +49,26 @@ void setup() {
   pinMode(RED_LED_PIN, OUTPUT);
   pinMode(GREEN_LED_PIN, OUTPUT);
   pinMode(SOUNDER_PIN, OUTPUT);
+}
 
+void initGsm() {
   gsmSerial.begin(9600);
   gsmSerial.println("AT"); // handshake
+}
 
+void initDhtSensor() {
   dht.begin();
-  lcd.begin(16, 2);
+}
+
+void initLcd() {
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop() {
+  readSensors();
+  displayInformation();
+
   if (isDangerousSituation()) {
     processDangerousSituation();
   } else if (isWet()) {
@@ -54,70 +77,56 @@ void loop() {
     processOkSituation();
   }
 
-  delay(200);
+  delay(100);
+}
+
+void readSensors() {
+  gasValue = digitalRead(GAS_SENSOR_PIN);
+  flameValue = digitalRead(FLAME_SENSOR_PIN);
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature();
+}
+
+void displayInformation() {
+  lcd.clear();
+
+  printOnLcd(0, 0, "Flame: " + String(flameValue));
+  printOnLcd(1, 0, "Gas: " + String(gasValue));
+  printOnLcd(2, 0, "Humidity: " + String(humidity));
+  printOnLcd(3, 0, "Temperature: " + String(temperature));
 }
 
 boolean isDangerousSituation() {
-  int flameValue = digitalRead(FLAME_SENSOR_PIN);
-  int gasValue = digitalRead(GAS_SENSOR_PIN);
-  Serial.print("Flame Value: ");
-  Serial.println(flameValue);
-  lcd.setCursor(0, 0);
-  lcd.print("Flame: ");
-  lcd.print(flameValue);
-  
-  Serial.print("Gas Value: ");
-  Serial.println(gasValue);
-  lcd.setCursor(0, 1);
-  lcd.print("Gas: ");
-  lcd.print(gasValue);
-
-  delay(100);
-
-  lcd.clear();
-
   return gasValue == HIGH || flameValue == HIGH;
 }
 
 boolean isWet() {
-  float humidity = dht.readHumidity();
-  float temperature = dht.readTemperature();
-  Serial.print("Humidity: ");
-  Serial.println(humidity);
-  lcd.setCursor(0, 0);
-  lcd.print("Hum: ");
-  lcd.print(humidity);
-  
-  Serial.print("Temperature: ");
-  Serial.println(temperature);
-  lcd.setCursor(0, 1);
-  lcd.print("Temp: ");
-  lcd.print(temperature);
-
-  delay(100);
-
-  lcd.clear();
-
   return humidity >= 70;
 }
 
+void printOnLcd(int row, int column, String text) {
+  lcd.setCursor(column, row);
+  lcd.print(text);
+}
+
 void processWetSituation() {
-  Serial.println("Wet!");
   digitalWrite(RED_LED_PIN, HIGH);
   digitalWrite(SOUNDER_PIN, HIGH);
   digitalWrite(GREEN_LED_PIN, LOW);
 }
 
 void processDangerousSituation() {
-  Serial.println("WARNING!");
   digitalWrite(RED_LED_PIN, HIGH);
   digitalWrite(SOUNDER_PIN, HIGH);
   digitalWrite(GREEN_LED_PIN, LOW);
+
+  lcd.clear();
+  printOnLcd(0, 0, "WARNING!");
+  printOnLcd(1, 0, "Propability of fire!");
   sendWarningMessage();
 }
 
 void processOkSituation() {
-  Serial.println("OK");
   digitalWrite(RED_LED_PIN, LOW);
   digitalWrite(SOUNDER_PIN, LOW);
   digitalWrite(GREEN_LED_PIN, HIGH);
@@ -127,8 +136,6 @@ void processOkSituation() {
 
 void sendWarningMessage() {
   if (!isSmsSent) {
-    Serial.println("Sending SMS to " + telephoneNumber);
-
     gsmSerial.println("AT+CMGF=1"); // Setup text mode
     gsmSerial.println("AT+CMGS=\"" + telephoneNumber + "\"\r");
     gsmSerial.println("Warning! Propability of fire!");
